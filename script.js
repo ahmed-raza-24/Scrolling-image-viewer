63a2581173dd0bed907009b0b42103871420439d
ahmed-raza-24 <ahmedrazalohar24@gmail.com>
2025-10-30 20:28:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let pic = document.querySelector(".images");
+let button = document.getElementById("btn");
+let index = 0;
+
+let totalImages = pic.querySelectorAll("img").length;
+button.addEventListener("click", function() {
+  index++;
+  if (index >= totalImages) {
+    index = 0;
+  }
+  const height = pic.clientHeight;
+  pic.scrollTo({
+    top: index * height,
+    behavior: "smooth"
+  });
+});
