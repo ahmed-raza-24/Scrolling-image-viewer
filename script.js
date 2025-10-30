50faa14b98a05ba44d0959a5c1773f64adb95ae6
ahmed-raza-24 <ahmedrazalohar24@gmail.com>
2025-10-30 20:42:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let pic = document.querySelector(".images");
 let button = document.getElementById("btn");
 let index = 0;
-//
 let totalImages = pic.querySelectorAll("img").length:
 button.addEventListener("click", function() {
   index++;
